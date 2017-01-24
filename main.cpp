f22e2a8b4813762d6f09133c8002ffeaeb602323
Muhammad Ahmmad <muhammad.ahmmad@mapscape.eu>
2017-01-24 01:27:46 +0100

    ddd

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 <!-- Author: Alexander Shvets (alex@githowto.com) -->
 <html>
   <head>
+  <sddsdsd>
     <!-- no style -->
+    <dsdsds>
   </head>
   <body>
-    <h1>Hello, World! Life is great!</h1> <!--- Test conflict_heeee>
+    <h1>Hello, World! Life is great!</h1> <!--- Test conflict_heeee><no2>
   </body>
 </html>
